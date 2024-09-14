fac2886e4a2d098cec8c1f3f76072a274fc92115
Nicolas Ramirez <nicolasramz216@gmail.com>
2024-09-13 21:52:23 -0500

    #1

---
+++ Parcial1-ll/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+
+    //Escriba un programa que pida los coeficientes de una ecuación de primer grado (a x + b = 0) y muestre la solución.
+    //Una ecuación de primer grado puede no tener solución, tener una solución única, o que todos los 
+    //números sean solución. La fórmula de las soluciones es x = -b / a
+    {
+        Console.WriteLine("Ingrese el valor de a: ");
+        decimal num1 = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de b: ");
+        Decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+        //
+
+    }
+
+   
+}
