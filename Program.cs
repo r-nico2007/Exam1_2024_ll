e5bcd347545143cc0a7196478face568b07fe157
Nicolas Ramirez <nicolasramz216@gmail.com>
2024-09-13 20:31:51 -0500

    #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    //Realizar un algoritmo con C# donde se solucione el siguiente problema: Un vendedor recibe un
+    //sueldo base de $XXXX.Además, recibe una comisión del 10% sobre el valor total de cada venta que
+    //realiza. El vendedor desea saber:
+    //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes?
+    //¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
+    //¿Cuál fue la venta que le generó la mayor comisión?
+    //¿Cuál fue el promedio de las comisiones que recibió por cada venta?
+
+    {
+
+    }
+}
